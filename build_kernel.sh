ffc097379f28b5af91c0e7eba30e7e04eda6f845
jamison904 <james.jamison85@gmail.com>
2015-01-25 11:11:20 -0500

    time to build

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,4 +1,7 @@
+time_start=$(date +%s.%N)
 mkdir $(pwd)/out
 make -C $(pwd) O=$(pwd)/out VARIANT_DEFCONFIG=apq8084_sec_trlte_tmo_defconfig apq8084_sec_defconfig SELINUX_DEFCONFIG=selinux_defconfig
 make -C $(pwd) O=$(pwd)/out
 cp $(pwd)/out/arch/arm/boot/zImage $(pwd)/arch/arm/boot/zImage
+time_end=$(date +%s.%N)
+echo -e "${BLDYLW}Total time elapsed: ${TCTCLR}${TXTGRN}$(echo "($time_end - $time_start) / 60"|bc ) ${TXTYLW}minutes${TXTGRN} ($(echo "$time_end - $time_start"|bc ) ${TXTYLW}seconds) ${TXTCLR}"
